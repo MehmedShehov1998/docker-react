7fd51b9eea028954de9211a26f0bd309c36da615
Mehmed Shehov <mehmedshehov@Mehmeds-MBP.deltasource.eu>
2021-10-14 15:55:33 +0300

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Mehmed Shehov Docker-React task</p>
+        <p>Mehmed Shehov Docker-React</p>
         <a
           className="App-link"
           href="https://reactjs.org"
