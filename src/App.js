b60eaedbf95344ca62c3d81741f5b1bccf78394a
Mehmed Shehov <mehmedshehov@Mehmeds-MBP.deltasource.eu>
2021-10-14 15:15:10 +0300

    changed the react app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Mehmed Shehov</p>
+        <p>Mehmed Shehov Docker-React task</p>
         <a
           className="App-link"
           href="https://reactjs.org"
